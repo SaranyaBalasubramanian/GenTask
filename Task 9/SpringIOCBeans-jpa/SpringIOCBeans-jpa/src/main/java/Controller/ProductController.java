48cc09aab212641f914b3f4b31a00a1dcd59b2e8
SaranyaBalasubramanian <160742887+SaranyaBalasubramanian@users.noreply.github.com>
2024-04-10 11:02:12 +0530

    Task 9
    Task 9 Completed

---
+++ Task 9/SpringIOCBeans-jpa/SpringIOCBeans-jpa/src/main/java/Controller/ProductController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import Domain.Product;
+import Service.ProductService;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/addProduct")
+    public String showAddProductForm() {
+        return "addProduct";
+    }
+
+    @PostMapping("/addProduct")
+    public String addProduct(@ModelAttribute Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/displayProduct")
+    public String showDisplayProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "displayProduct";
+    }
+
+    @PostMapping("/displayProduct")
+    public String displayProduct(@RequestParam("category") String category, Model model) {
+        List<Product> products = productService.getAllProductsByCategory(category);
+        model.addAttribute("products", products);
+        return "productList";
+    }
+}
---
+++ Task 9/SpringIOCBeans-jpa/SpringIOCBeans-jpa/src/test/java/SpringIOCBeansjpa/SpringIOCBeansjpa/SpringIocBeansJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringIOCBeansjpa.SpringIOCBeansjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocBeansJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
